ec78781c79e3e9907127aa1de7b8d81cbc6cfb51
elihawkins123 <elizabethrhawkins123@gmail.com>
2023-12-22 21:59:31 -0700

    updated models product, tag, and product tag

--- models/Category.js
+++ models/Category.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
+const ProductTag = require('./ProductTag.js');
 
 class Category extends Model {}
 
@@ -26,5 +27,120 @@ Category.init(
     modelName: 'category',
   }
 );
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true, 
+    }, 
+    product_name: {
+      type: DataTypes.STRING, 
+      allowNull: false,
+    }, 
+    price: {
+      type: DataTypes.DECIMAL, 
+      allowNull: false, 
+      validate: {
+        isDecimal: true,
+      },
+    }, 
+    stock: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      defaultValue: 10, 
+      validate: {
+        isNumeric: true,
+      },
+    }, 
+    category_id: {
+      type: DataTypes.INTEGER, 
+      references: {
+        model: 'category', 
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+Tag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true, 
+    }, 
+    tag_name: {
+      type: DataTypes.STRING, 
+    }, 
+    category_id: {
+      type: DataTypes.INTEGER, 
+      references: {
+        model: 'category', 
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+ProductTag.init(  
+  {
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true, 
+    }, 
+    product_id: {
+      type: DataTypes.INTEGER, 
+      references: {
+        model: 'product', 
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER, 
+      references: {
+        model: 'tag', 
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);  
+Product.hasMany(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+Category.belongsTo(Product, {
+  foreignKey: 'category_id',
+});
+Product.hasMany(Tag, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+}); 
+Tag.belongsTo(Product, {
+  foreignKey: 'category_id',
+}); 
 
 module.exports = Category;
